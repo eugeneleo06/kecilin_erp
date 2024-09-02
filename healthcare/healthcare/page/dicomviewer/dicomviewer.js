dea881218699e8fede525852c84fe95f2c01800a
eugeneleo06 <eugeneleo14@gmail.com>
2024-09-02 13:02:22 +0700

    redirect

--- healthcare/healthcare/page/dicomviewer/dicomviewer.js
+++ healthcare/healthcare/page/dicomviewer/dicomviewer.js
@@ -1,19 +1,3 @@
-// In /home/frappe/frappe-bench/apps/healthcare/healthcare/healthcare/page/dicomviewer/dicomviewer.js
-
 frappe.pages['dicomviewer'].on_page_load = function(wrapper) {
-    var page = frappe.ui.make_app_page({
-        parent: wrapper,
-        title: 'DICOM Viewer',
-        single_column: true
-    });
-    $(page.body).html(`
-        <div>
-            <h1>DICOM Viewer</h1>
-            <div id="dicom-container">
-                Test
-            </div>
-        </div>
-    `);
-    // Initialize your DICOM viewer here
-    console.log('DICOM Viewer initialized');
+    window.location.href = 'http://34.101.82.205:5000/';
 };
